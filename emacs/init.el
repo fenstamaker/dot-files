254ddc05295856da2c43d3cb1576ecca09544e01
fenstamaker <gary@fenstamaker.com>
2017-09-09 21:28:27 -0400

    Emacs changes

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,13 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;;; Package Bootstrapping
 
@@ -142,17 +149,17 @@
   (setq-default save-place t)
   (setq save-place-file (expand-file-name "places" user-emacs-directory)))
 
-(use-package undo-tree
-  :ensure t
-  :diminish undo-tree-mode
-  :bind
-  (("s-u" . undo-tree-visualize)
-   ("s-z" . undo)
-   ("s-Z" . undo-tree-redo))
-  :config
-  (global-undo-tree-mode t)
-  (setq undo-tree-visualizer-timestamps t)
-  (setq undo-tree-visualizer-diff t))
+;; (use-package undo-tree
+;;   :ensure t
+;;   :diminish undo-tree-mode
+;;   :bind
+;;   (("s-u" . undo-tree-visualize)
+;;    ("s-z" . undo)
+;;    ("s-Z" . undo-tree-redo))
+;;   :config
+;;   (global-undo-tree-mode t)
+;;   (setq undo-tree-visualizer-timestamps t)
+;;   (setq undo-tree-visualizer-diff t))
 
 (use-package company
   :ensure t
@@ -166,7 +173,7 @@
   (add-to-list 'company-dabbrev-code-modes 'clojure-mode)
 
   (setq company-idle-delay 0.125)
-  (setq company-minimum-prefix-length 1))
+  (setq company-minimum-prefix-length 3))
 
 (use-package exec-path-from-shell
   :ensure t
@@ -196,7 +203,12 @@
   :config
   (smartparens-global-mode t)
   (show-smartparens-global-mode t)
-  (setq show-paren-style 'expression))
+  (setq show-paren-style 'expression)
+
+  (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+  (sp-local-pair 'clojure-mode "'" nil :actions nil)
+  (sp-local-pair 'cider-repl-mode "'" nil :actions nil)
+  )
 
 ;; (use-package parinfer
 ;;   :ensure t
@@ -306,20 +318,58 @@
 
 ;;; Package modes
 
-(use-package cider
+;; (use-package cider
+;;   :ensure t
+;;   :config
+;;   (cider-mode t)
+;;   (add-hook 'cider-mode-hook #'eldoc-mode)
+
+;;   (setq cider-repl-use-pretty-printing t)
+;;   (setq cider-repl-use-clojure-font-lock t)
+;;   (setq cider-show-error-buffer nil)
+;;   (setq cider-prompt-save-file-on-load nil)
+
+;;   (global-set-key (kbd "s-r")       'cider-refresh)
+;;   (add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
+;;   (add-hook 'cider-repl-mode-hook #'company-mode)
+;;   (add-hook 'cider-mode-hook #'company-mode))
+
+(defun reload-current-clj-ns (next-p)
+  (interactive "P")
+  (let ((ns (clojure-find-ns)))
+    (message (format "Loading %s ..." ns))
+    (inf-clojure-eval-string (format "(require '%s :reload)" ns))
+    (when (not next-p) (inf-clojure-eval-string (format "(in-ns '%s)" ns)))))
+
+(defun find-tag-without-ns (next-p)
+  (interactive "P")
+  (find-tag (first (last (split-string (symbol-name (symbol-at-point)) "/")))
+            next-p))
+
+(defun erase-inf-buffer ()
+  (interactive)
+  (with-current-buffer (get-buffer "*inf-clojure*")
+    (erase-buffer))
+  (inf-clojure-eval-string ""))
+
+
+(use-package inf-clojure
   :ensure t
   :config
-  (cider-mode t)
-  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (setq inf-clojure-prompt-read-only nil)
+  (add-hook 'inf-clojure-minor-mode-hook
+            (lambda () (setq completion-at-point-functions nil)))
+  (add-hook 'clojure-mode-hook 'inf-clojure-minor-mode)
 
-  (setq cider-repl-use-pretty-printing t)
-  (setq cider-repl-use-clojure-font-lock t)
-  (setq cider-show-error-buffer nil)
-  (setq cider-prompt-save-file-on-load nil)
-
-  (add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
-  (add-hook 'cider-repl-mode-hook #'company-mode)
-  (add-hook 'cider-mode-hook #'company-mode))
+  (add-hook 'clojure-mode-hook
+            '(lambda ()
+               (define-key clojure-mode-map "\C-c\C-k" 'reload-current-clj-ns)
+               (define-key clojure-mode-map "\M-." 'find-tag-without-ns)
+               (define-key clojure-mode-map "\C-cl" 'erase-inf-buffer)
+               (define-key clojure-mode-map "\C-c\C-t" 'clojure-toggle-keyword-string)))
+  (add-hook 'inf-clojure-mode-hook
+            '(lambda ()
+               (define-key inf-clojure-mode-map "\C-cl" 'erase-inf-buffer))))
 
 (use-package clojure-mode
   :ensure t
@@ -335,7 +385,20 @@
     (context 2)
     (fact 1)
     (safe->> 1)
-    (safe-> 1)))
+    (safe-> 1)
+    (fdef 1)
+    ))
+
+(use-package ensime
+  :ensure t
+  :config
+  (add-to-list 'exec-path "/usr/local/bin"))
+
+(use-package sbt-mode
+  :ensure t)
+
+(use-package scala-mode
+  :ensure t)
 
 (use-package web-mode
   :ensure t
@@ -490,9 +553,88 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#1e1e1e" "#cf6a4c" "#8f9d6a" "#f9ee98" "#7587a6" "#9b859d" "#7587a6" "#a7a7a7"])
+ '(ansi-term-color-vector
+   [unspecified "#1e1e1e" "#cf6a4c" "#8f9d6a" "#f9ee98" "#7587a6" "#9b859d" "#7587a6" "#a7a7a7"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("cea3ec09c821b7eaf235882e6555c3ffa2fd23de92459751e18f26ad035d2142" "8be07a2c1b3a7300860c7a65c0ad148be6d127671be04d3d2120f1ac541ac103" "78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" "25c242b3c808f38b0389879b9cba325fb1fa81a0a5e61ac7cae8da9a32e2811b" "60e09d2e58343186a59d9ed52a9b13d822a174b33f20bdc1d4abb86e6b17f45b" "c9f102cf31165896631747fd20a0ca0b9c64ecae019ce5c2786713a5b7d6315e" "25c06a000382b6239999582dfa2b81cc0649f3897b394a75ad5a670329600b45" "7bef2d39bac784626f1635bd83693fae091f04ccac6b362e0405abf16a32230c" "3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "0c3b1358ea01895e56d1c0193f72559449462e5952bded28c81a8e09b53f103f" "ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" "196df8815910c1a3422b5f7c1f45a72edfa851f6a1d672b7b727d9551bb7c7ba" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(fci-rule-color "#eee8d5")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+ '(hl-sexp-background-color "#efebe9")
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (prettier-js rjsx-mode imenu+ imenu-anywhere multiple-cursors helm-swoop helm-projectile projectile elisp--witness--lisp markdown-mode web-mode cider helm smartparens rainbow-delimiters flycheck company undo-tree use-package))))
+    (base16-theme inf-clojure ensime color-theme-solarized material-theme dracula-theme solarized-theme color-theme-sanityinc-solarized prettier-js rjsx-mode imenu+ imenu-anywhere multiple-cursors helm-swoop helm-projectile projectile elisp--witness--lisp markdown-mode web-mode helm smartparens rainbow-delimiters flycheck company use-package)))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#cb4b16")
+     (60 . "#b58900")
+     (80 . "#859900")
+     (100 . "#2aa198")
+     (120 . "#268bd2")
+     (140 . "#d33682")
+     (160 . "#6c71c4")
+     (180 . "#dc322f")
+     (200 . "#cb4b16")
+     (220 . "#b58900")
+     (240 . "#859900")
+     (260 . "#2aa198")
+     (280 . "#268bd2")
+     (300 . "#d33682")
+     (320 . "#6c71c4")
+     (340 . "#dc322f")
+     (360 . "#cb4b16"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
